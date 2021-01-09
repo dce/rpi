18553c074440a6f10f2f66a597afc96ebc3c3943
David Eisinger <david.eisinger@gmail.com>
2021-01-08 21:50:58 -0500

    Just messing around

--- epd7in5/epd7in5.go
+++ epd7in5/epd7in5.go
@@ -299,7 +299,11 @@ func (e *Epd) Display(img []byte) {
 	e.sendCommand(0x24);
 
 	for i := 0; i < e.heightByte * e.widthByte / 8; i++ {
-		e.sendData(img[i])
+    if (i % 2 == 0) {
+      e.sendData(0xFF)
+    } else {
+      e.sendData(0x00)
+    }
 	}
 
 	e.sendCommand(0x22);
