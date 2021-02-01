82e4f5335bbaba956bc0bd8018eb8d942d36328e
David Eisinger <david.eisinger@gmail.com>
2021-02-01 11:40:33 -0500

    Revert blocksize change

--- epd7in5/epd7in5.go
+++ epd7in5/epd7in5.go
@@ -196,7 +196,7 @@ func(e *Epd) sendData2(data []byte) {
 	e.cs.Out(gpio.Low)
 
 	length := len(data)
-	blocksize := 1
+	blocksize := 4096
 
 	for start := 0; start < length; start += blocksize {
 		end := start + blocksize
