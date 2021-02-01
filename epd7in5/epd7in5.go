310de660910644cce58ea9546554bc7743a4dc5d
David Eisinger <david.eisinger@gmail.com>
2021-02-01 11:29:18 -0500

    Try blocksize of 1

--- epd7in5/epd7in5.go
+++ epd7in5/epd7in5.go
@@ -196,7 +196,7 @@ func(e *Epd) sendData2(data []byte) {
 	e.cs.Out(gpio.Low)
 
 	length := len(data)
-	blocksize := 880 * 4
+	blocksize := 1
 
 	for start := 0; start < length; start += blocksize {
 		end := start + blocksize
